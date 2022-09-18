a0ec6a2271e999f90ef3df8ec7b9eea3e4af0be2
Liam <101684827+SkiingIsFun123@users.noreply.github.com>
2022-09-18 16:32:30 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,6 +3,8 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   document.getElementById("city").innerHTML = message.city;
   document.getElementById("link").href = "https://www.google.com/maps/search/" + message.street + " " + message.city + "/";
   document.getElementById("link").style.display = "block";
+  document.getElementById("maps").src = "https://www.google.com/maps?q=" + message.street + " " + message.city + "&output=embed";
+  document.getElementById("maps").style.display = "block";
   sendResponse({
     data: "Response from popup.js",
   });
